350a16ce93c5d68fe66ae85fb17e17d4bb1d8f54
etuong <etuong@gmail.com>
2022-07-29 11:41:23 -0700

    Init commit on client code

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Chat } from "./chat/Chat";
+import "./index.scss";
+import Test from "./Test";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
